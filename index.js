a9e4b46b3260e821cf7564ee31ef35bdd461b0b5
daham24 <bhanukadaham98@gmail.com>
2024-04-03 17:06:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+* express (npm i express)
+* mongoose (npm i mongoose)
+* nodemon (sudo npm i nodemon -g)
+* dotenv (npm i dotenv) -> sendgrid
+* body-parser (npm i body-parser)
+* bcrypt (npm i bcrypt)
+* json web token (npm i jsonwebtoken)
+*/
